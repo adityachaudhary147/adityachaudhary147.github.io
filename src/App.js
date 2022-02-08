5569d8feb80a99454e8d3490b692cfb185d928f8
aditya chaudhary <chaudharyaditya.in@gmail.com>
2022-02-08 16:33:49 +0530

    here to live forever

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+import Foot from './Foot'
+import './App.css';
+import About from './About';
+export default function App() {
+  return <div>
+      <Nav />
+      <About />
+      <Foot />
+  </div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-
-ReactDOM.render(<h1>Aditya Chaudhary</h1>,document.getElementById('root'));
+import App from "./App";
+ReactDOM.render(<App />,document.getElementById('root'));
